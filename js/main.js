16469affce981448b3e37278bb968293caefe918
Luke Dillon <lukedillon@Users-MacBook-Air.local>
2019-03-09 15:46:26 -0800

    fixed some fonts, button, etc

--- js/main.js
+++ js/main.js
@@ -1,16 +1,17 @@
 var slideIndex = 1;
-showSlides(slideIndex);
+// showSlides(slideIndex);
 
 // Next/previous controls
 function plusSlides(n) {
-  showSlides(slideIndex += n);
+  // showSlides(slideIndex += n);
 }
 
 // Thumbnail image controls
 function currentSlide(n) {
-  showSlides(slideIndex = n);
+  // showSlides(slideIndex = n);
 }
 
+/*
 function showSlides(n) {
   var i;
   var slides = document.getElementsByClassName("mySlides");
@@ -25,4 +26,41 @@ function showSlides(n) {
   }
   slides[slideIndex-1].style.display = "block"; 
   dots[slideIndex-1].className += " active";
+}
+*/
+
+/* When the user clicks on the button, 
+toggle between hiding and showing the dropdown content */
+isOpen = 0;
+function myFunction() {
+  if (!isOpen)
+  {
+    document.getElementById("myDropdown").classList.add("show");
+    isOpen = 1;
+    console.log("Open!");
+  }
+  
+  else
+  {
+    document.getElementById("myDropdown").classList.remove("show");
+    isOpen = 0;
+    console.log("Closed!");
+  }
+  
+}
+
+// Close the dropdown menu if the user clicks outside of it
+
+window.onclick = function(event) 
+{
+  if (!event.target.matches('.dropbtn')) 
+  {
+    if(isOpen)
+    {
+      document.getElementById("myDropdown").classList.remove("show");
+      isOpen = 0;
+      console.log("Closed!");
+    }
+    
+  }
 }
